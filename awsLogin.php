c1196ff54cdbbb8c2fb3460d020081e7ed4a379f
brimason81 <34803524+brimason81@users.noreply.github.com>
2020-11-25 20:45:25 -0500

    Delete awsLogin.php

--- awsLogin.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // Database Login Credentials
-    $db_host = "rds-mysql-tinyurl.c94o0cssdqsy.us-east-2.rds.amazonaws.com";
-    $db_database = "tinyURL";
-    $db_user = "brimasonAdmin";
-    $db_password = "CoronaVirus2020";
-?>
