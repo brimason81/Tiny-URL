5c5374ef27e64c18421aa7189afe5d53ade19341
brimason81 <34803524+brimason81@users.noreply.github.com>
2020-11-25 20:43:47 -0500

    Add files via upload

---
+++ awsLogin.php
@@ -0,0 +1,7 @@
+<?php
+    // Database Login Credentials
+    $db_host = "rds-mysql-tinyurl.c94o0cssdqsy.us-east-2.rds.amazonaws.com";
+    $db_database = "tinyURL";
+    $db_user = "brimasonAdmin";
+    $db_password = "CoronaVirus2020";
+?>
